e0a44dfc9001966e814bc505f212981b23ca17c7
Justin Stone <justone@davidson.edu>
2024-02-27 19:11:16 -0500

    Updated mistakes on my scraper for assignment 6

--- in-class-6/sp500_scraper/sp500_scraper/spiders/performance.py
+++ in-class-6/sp500_scraper/sp500_scraper/spiders/performance.py
@@ -7,12 +7,12 @@ class PerformanceSpider(scrapy.Spider):
     start_urls = ["http://slickcharts.com/sp500/performance"]
 
     def parse(self, response):
-        for row in response.xpath('//table[@class="table table-hover table-borderless table-sm"]/tbody/tr'):
+        for row in response.xpath('//tr'):
             yield {
                 'number': row.xpath('td[1]/text()').get(),
                 'company': row.xpath('td[2]/a/text()').get(),
-                'symbol': row.xpath('td[3]/text()').get(),
-                'ytd_return': row.xpath('td[6]/text()').get(),
+                'symbol': row.xpath('td[3]/a/text()').get(),
+                'ytd_return': row.xpath('td[4]/text()').get(),
             }
-            
+
     
